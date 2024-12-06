62082572342148f5a8e71a1b153006b8a8a6a603
Etanielcaus <nielfilosofia@gmail.com>
2024-12-05 23:13:09 -0300

    first commit

---
+++ src/main/java/com/cursoudemy/PostGresConnectionSpring/PostGresConnectionSpringApplication.java
@@ -0,0 +1,13 @@
+package com.cursoudemy.PostGresConnectionSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostGresConnectionSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostGresConnectionSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursoudemy/PostGresConnectionSpring/PostGresConnectionSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursoudemy.PostGresConnectionSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostGresConnectionSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
