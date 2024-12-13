1de570b05337fd92fd238cee39fad9441c7ca11d
Etanielcaus <nielfilosofia@gmail.com>
2024-12-12 23:08:32 -0300

    Change a ModelMapper for DTO

--- src/main/java/com/cursoudemy/PostGresConnectionSpring/mapper/DozerMapper.java
+++ src/main/java/com/cursoudemy/PostGresConnectionSpring/mapper/DozerMapper.java
@@ -1,16 +1,15 @@
 package com.cursoudemy.PostGresConnectionSpring.mapper;
 
-import com.github.dozermapper.core.DozerBeanMapperBuilder;
-import com.github.dozermapper.core.Mapper;
+import org.modelmapper.ModelMapper;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class DozerMapper {
-    private static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+    private static ModelMapper mapper = new ModelMapper();
 
-    public static <O, D> D parseObject(O origin, Class<D> destination){
-        return mapper.map(origin,destination);
+    public static <O, D> D parseObject(O origin, Class<D> destination) {
+        return mapper.map(origin, destination);
     }
 
     public static <O, D> List<D> parseListObjects(List<O> origin, Class<D> destination) {
